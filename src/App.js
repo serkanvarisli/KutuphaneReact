213a0cbeb307ec397ef99d787e3cb71cff7377ce
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-03-30 00:35:51 +0300

    React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import './App.css';
-import Data from './Data.json';
-
+import "./App.css";
+import Data from "./Data.json";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [showClear, setShowClear] = useState(false);
-function handleClear() {
+  function handleClear() {
     setSearchTerm("");
     setShowClear(false);
   }
@@ -16,50 +15,61 @@ function handleClear() {
   }
 
   return (
-    <div className='maincontainer'>
-      <h1>KÜTÜPHANE</h1>  
-      <div className='inputContainer'>
-        <input className='search' id='search-box' type="text" placeholder="Kitap veya Yazar Ara..."
+    <div className="maincontainer">
+      <h1>KÜTÜPHANE</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          id="search-box"
+          type="text"
+          placeholder="Kitap veya Yazar Ara..."
           onChange={handleSearch}
           //clear input
-          value={searchTerm} 
+          value={searchTerm}
         />
-        {showClear&&<button className='clear' onClick={handleClear}>Temizle</button>}
+        {showClear && (
+          <button className="clear" onClick={handleClear}>
+            Temizle
+          </button>
+        )}
       </div>
-      <div className='dataContainer'>
-        {
-          Data
-            .filter((val) => {
-              if (searchTerm == "") {    
-                return val;
-              } else if (val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-                return val;
-              }
-              else if (val.author.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-                return val;
-              }
-            }).map((val) => {
-              return (
-                <div className="data" key={val.id}>
+      <div className="dataContainer">
+        {Data.filter((val) => {
+          if (searchTerm == "") {
+            return val;
+          } else if (
+            val.title
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return val;
+          } else if (
+            val.author
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return val;
+          }
+        }).map((val) => {
+          return (
+            <div className="data" key={val.id}>
+              <img src={val.image} alt="" />
+              <h3>{val.title}</h3>
+              <b>{val.author}</b>
+              <p>{val.Description}</p>
+            </div>
+          );
+        })}
+      </div>
 
-                  <img src={val.image} alt="" />
-                  <h3>{val.title}</h3>
-                  <b>{val.author}</b>
-                  <p>{val.Description}</p>
-                </div>
-              )
-            })
-        }
-      </div>
       <br />
       <br />
       <br />
-      <div className='footer'>
+      <div className="footer">
         <p>© Copyright 2023 Serkan Varışlı</p>
       </div>
     </div>
   );
 }
-
 
 export default App;
